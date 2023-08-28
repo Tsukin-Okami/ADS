17d4da4626aeae033499afb2e4cd29639241541b
JhonikGames <69688931+JhonikGames@users.noreply.github.com>
2023-08-28 14:09:29 -0300

    Update ADSLib.c

--- Bibliotecas/ADSLib.c
+++ Bibliotecas/ADSLib.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+/* 
+    - Modulo de Bibliotecas -
+  Tipos inclusos:
+> ADS_SORT = organiza vetor numeral em ordem decrescente
+> ADS_
+*/
+#define prefix ADS
 
+void organizar_notas()
+{
+	for(int i=0; i<qtd; i++)
+    {
+        for(int j=i+1; j<qtd; j++) { if(nota_provas[i]>nota_provas[j])
+            {
+                float temp = nota_provas[i];
+                nota_provas[i] = nota_provas[j];
+                nota_provas[j] = temp;
+            }
+        }
+    }
+}
+// --- main --- 
+int main()
+{
+  // intro
+  puts("Biblioteca ADSLib Inicializada");
+  // chamada
+  
+  // retorno
+  return 0;
+}
