f36a99b59a80b19f84b64f826e2e1811c08f9627
JhonikGames <69688931+JhonikGames@users.noreply.github.com>
2023-08-31 14:45:50 -0300

    Update DesvioPadrao.c

--- Eletronica/DesvioPadrao.c
+++ Eletronica/DesvioPadrao.c
@@ -19,64 +19,33 @@ int main()
 		scanf("%f",&conjunto[i]);
 	}
 	
-	for(int i=0;i<size;i++)
-	{
-		printf("%.2f, ",conjunto[i]);
-	}
-	putchar('\n');
-	
-	// 			- calculo -
-	
 	// passo 1
 	
 	float soma_cj=0;
-	
 	for(int i=0;i<size;i++)
 	{
 		soma_cj += conjunto[i];
 	}
-	
 	float res_cj = soma_cj/size;
-	
-	// imprime
-	
-	printf("Resultado da media do conjunto: %.2f\n",res_cj);
-	
+
 	// passo 2
-	
+
 	float conjunto_diff[size];
-	
 	for(int i=0;i<size;i++)
 	{
 		conjunto_diff[i] = pow((conjunto[i]-res_cj),2);
 	}
 	
-	// imprime
-	
-	for(int i=0;i<size;i++)
-	{
-		printf("%.2f, ",conjunto_diff[i]);
-	}
-	putchar('\n');
-	
 	// passo 3
-	
 	float soma_diff=0;
-	
 	for(int i=0;i<size;i++)
 	{
 		soma_diff += conjunto_diff[i];
 	}
-	
 	float res_diff = soma_diff/size;
-	
-	// imprime
-	
-	printf("Varianca do conjunto: %.2f\n",res_diff);
-	
-	// passo 4
-	
 	float desvio = sqrt(res_diff);
+	
+	// fim
 	
 	printf("Desvio padrao: %.2f",desvio);
 	
