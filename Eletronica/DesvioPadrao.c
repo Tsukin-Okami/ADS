e717fba47355c8b96c9c4a559720a7df5224e1b8
JhonikGames <69688931+JhonikGames@users.noreply.github.com>
2023-08-31 14:55:52 -0300

    Update DesvioPadrao.c

--- Eletronica/DesvioPadrao.c
+++ Eletronica/DesvioPadrao.c
@@ -2,52 +2,58 @@
 #include <stdlib.h>
 #include <math.h>
 
-// desvio padrao
+// Desvio Padrao
 
-int main()
-{
-	int size;
-	
-	printf("Digite o tamanho do conjunto: ");
-	scanf("%d",&size);
-	
-	float conjunto[size];
-	
-	for(int i=0;i<size;i++)
-	{
-		printf("Digite o valor na %da posicao: ",i+1);
-		scanf("%f",&conjunto[i]);
-	}
-	
-	// passo 1
-	
-	float soma_cj=0;
-	for(int i=0;i<size;i++)
-	{
-		soma_cj += conjunto[i];
-	}
-	float res_cj = soma_cj/size;
+#define MAX_SIZE 100  // Defina o tamanho máximo do conjunto
 
-	// passo 2
+// Função para calcular a média de um conjunto de valores
+float calcularMedia(const float conjunto[], int size) {
+    float soma = 0;
+    for (int i = 0; i < size; i++) {
+        soma += conjunto[i];
+    }
+    return soma / size;
+}
 
-	float conjunto_diff[size];
-	for(int i=0;i<size;i++)
-	{
-		conjunto_diff[i] = pow((conjunto[i]-res_cj),2);
-	}
-	
-	// passo 3
-	float soma_diff=0;
-	for(int i=0;i<size;i++)
-	{
-		soma_diff += conjunto_diff[i];
-	}
-	float res_diff = soma_diff/size;
-	float desvio = sqrt(res_diff);
-	
-	// fim
-	
-	printf("Desvio padrao: %.2f",desvio);
-	
-	return 0;
+// Função para calcular o desvio padrão de um conjunto de valores
+float calcularDesvioPadrao(const float conjunto[], int size, float media) {
+    float somaDiff = 0;
+    for (int i = 0; i < size; i++) {
+        float diff = conjunto[i] - media;
+        somaDiff += diff * diff;
+    }
+    float resDiff = somaDiff / size;
+    return sqrt(resDiff);
+}
+
+int main() {
+    int size;
+
+    printf("Digite o tamanho do conjunto: ");
+    scanf("%d", &size);
+
+    if (size <= 0 || size > MAX_SIZE) {
+        printf("Tamanho invalido.\n");
+        return 1;
+    }
+
+    float *conjunto = (float*)malloc(size*sizeof(float));
+    if (conjunto == NULL) {
+        printf("Erro de alocacao de memoria.\n");
+        return 1;
+    }
+
+    for (int i=0; i<size; i++) {
+        printf("Digite o valor na %da posicao: ", i+1);
+        scanf("%f", &conjunto[i]);
+    }
+
+    float media = calcularMedia(conjunto, size);
+    float desvioPadrao = calcularDesvioPadrao(conjunto, size, media);
+
+    printf("Desvio padrao: %.2f\n", desvioPadrao);
+
+    free(conjunto);
+
+    return 0;
 }
