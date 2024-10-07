6f4ae86c75622d7395320698c4125bd183578beb
Tsukin-Okami <jhonikauanmb@gmail.com>
2024-10-07 17:12:28 -0300

    07.10.2024

--- Programas_PHP/QueryObject/FactoryPattern/TConnection.class.php
+++ Programas_PHP/QueryObject/FactoryPattern/TConnection.class.php
@@ -1,6 +1,34 @@
 <?php
 
-abstract class TConnection
+/**
+ * Classe TConnection
+ * gerencia conexões com o banco de dados através de arquivos de configuração 
+*/
+
+final class TConnection
 {
-    
+    /**
+     * Método __construct()
+     * não existirão instancias de TConnection, por isso estamos marcando-o como private
+    */
+
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * Método open()
+     * recebe o nome do banco de dados e instancia o objeto PDO correspondente
+     * @param string $name
+    */
+
+    public static function open($name) {
+        // verifica se existe arquivo de configuração para este banco de dados
+        if (file_exists("app.config/{$name}.ini"))
+        {
+            // le o INI e retorna um Array
+            $db = parse_ini_file("app.config/{$name}.ini");
+        }
+    }
 }
---
+++ Programas_PHP/QueryObject/FactoryPattern/test.php
@@ -0,0 +1,5 @@
+<?php
+
+$ini = parse_ini_file("app.config/livro.ini");
+
+print_r($ini);
