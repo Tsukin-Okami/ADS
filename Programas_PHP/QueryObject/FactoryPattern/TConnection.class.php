89d7fb6188e4f65a94b40eba18930c34c98a87db
Tsukin-Okami <jhonikauanmb@gmail.com>
2024-10-09 14:34:29 -0300

    09.10.2024 v2

--- Programas_PHP/QueryObject/FactoryPattern/TConnection.class.php
+++ Programas_PHP/QueryObject/FactoryPattern/TConnection.class.php
@@ -30,5 +30,63 @@ final class TConnection
             // le o INI e retorna um Array
             $db = parse_ini_file("app.config/{$name}.ini");
         }
+        else
+        {
+            // se não existir, lança um erro
+            throw new Exception("Arquivo.'$name' não encontrado");
+        }
+
+        // lê os arquivos e grava as informações contidas neles
+        $user = isset($db['user']) ? $db['user'] : 'NULL';
+        $pass = isset($db['pass']) ? $db['pass'] : 'NULL';
+        $name = isset($db['name']) ? $db['name'] : 'NULL';
+        $host = isset($db['host']) ? $db['host'] : 'NULL';
+        $type = isset($db['type']) ? $db['type'] : 'NULL';
+        $port = isset($db['port']) ? $db['port'] : 'NULL';
+
+        // descobre qual o tipo de (driver) banco de dados a ser utilizado
+        switch ($type) {
+            case 'pgsql':
+                $port = $port ? $port : '5432';
+
+                $conn = new PDO("pgsql:dbname={$name}; user={$user}; password={$pass}; host=$host; port={$port}");
+                break;
+            
+            case 'mysql':
+                $port = $port ? $port : '3306';
+
+                $conn = new PDO("mysql:host={$host}; port={$port}; dbname={$name}", $user, $pass);
+                break;
+
+            case 'ibase':
+                $port = $port ? $port : '3306';
+
+                $conn = new PDO("firebird:dbname={$name}", $user, $pass);
+                break;
+
+            case 'oci8':
+                $port = $port ? $port : '3306';
+                
+                $conn = new PDO("oci8:dbname={$name}", $user, $pass);
+                break;
+
+            case 'mssql':
+                $port = $port ? $port : '1433';
+                
+                $conn = new PDO("mssql:host={$host}, {$port}; dbname={$name}", $user, $pass);
+                break;
+
+            default:
+                # code...
+                throw new Exception("driver de banco de dados não reconhecido");
+        }
+
+        // define para que o PDO lance excessões na ocorrências de erros
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // retorna o objeto instanciado
+        return $conn;
     }
+
+
 }
--- Programas_PHP/QueryObject/FactoryPattern/test.php
+++ Programas_PHP/QueryObject/FactoryPattern/test.php
@@ -2,14 +2,4 @@
 
 $ini = parse_ini_file("app.config/livro.ini");
 
-print_r($ini); // aaa -- aaa >> aaa
-
-/*
-
-// aaa
-
--- aaa : lua
-
--! aaa
-
-*/
+print_r($ini);
